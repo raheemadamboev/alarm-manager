16b8b42677ea5e8b5c6e0ad5b64db23c7aa54075
raheemadamboev <vividricemaryandmax@gmail.com>
2023-01-14 15:42:33 +0500

    Created notification channel;

--- app/src/main/java/xyz/teamgravity/alarmmanager/injection/app/App.kt
+++ app/src/main/java/xyz/teamgravity/alarmmanager/injection/app/App.kt
@@ -2,6 +2,18 @@ package xyz.teamgravity.alarmmanager.injection.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import xyz.teamgravity.alarmmanager.core.notification.ReminderNotification
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    @Inject
+    lateinit var notification: ReminderNotification
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notification.createChannel()
+    }
+}
